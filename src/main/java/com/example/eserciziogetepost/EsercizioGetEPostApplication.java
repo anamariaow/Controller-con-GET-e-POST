495a194c2dfbd7d3641f841cd381b527b5adcd77
anamariaow <new_annemarie@yahoo.com>
2024-02-22 15:03:04 +0100

    esercizio con GET e POST name e name in reverse

---
+++ src/main/java/com/example/eserciziogetepost/EsercizioGetEPostApplication.java
@@ -0,0 +1,13 @@
+package com.example.eserciziogetepost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsercizioGetEPostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsercizioGetEPostApplication.class, args);
+	}
+
+}
